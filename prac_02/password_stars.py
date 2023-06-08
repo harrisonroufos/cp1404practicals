54448e29d2f3d6f04ee06ced8dcf5b282fdcfd66
harrisonroufos <harrison.roufos@my.jcu.edu.au>
2023-06-08 18:26:34 +1000

    Refactor password check program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -3,21 +3,23 @@ MINIMUM_PASSWORD_LENGTH = 8
 
 
 def main():
-    password = get_valid_password()
-    print_stars(len(password))
-
-
-def get_valid_password():
-    """Get valid password"""
-    password = input("Password: ")
+    """Print password length in stars"""
+    password = get_password()
     while len(password) < MINIMUM_PASSWORD_LENGTH:
         print("To short.")
         password = input("Password: ")
+    print_stars(len(password))
+
+
+def get_password():
+    """Get password"""
+    password = input("Password: ")
     return password
 
 
-def print_stars(length):
-    print("*" * length)
+def print_stars(password):
+    """Print stars"""
+    print("*" * password)
 
 
 main()
