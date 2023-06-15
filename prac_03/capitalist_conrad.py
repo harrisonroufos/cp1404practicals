dbe5278fc054cf52e3e90c5ecc178467bbc53839
harrisonroufos <harrison.roufos@my.jcu.edu.au>
2023-06-15 18:28:21 +1000

    Add output file and fix formatting for capitalist conrad program

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -9,14 +9,18 @@ The price should be displayed to the nearest cent (e.g. $33.59, not $33.59182329
 """
 import random
 
-MAX_INCREASE = 0.1  # 10%
+MAX_INCREASE = 0.175  # 10%
 MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 0.01
-MAX_PRICE = 1000.0
+MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
+OUTPUT_FILE = "stock_simulation.txt"
 
 price = INITIAL_PRICE
-print(f"${price:,.2f}")
+print(f"Starting price: ${price:,.2f}")
+
+number_of_days = 0
+out_file = open(OUTPUT_FILE, 'w')
 
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
@@ -31,5 +35,8 @@ while MIN_PRICE <= price <= MAX_PRICE:
         # between negative MAX_DECREASE and 0
         price_change = random.uniform(-MAX_DECREASE, 0)
 
+    number_of_days = number_of_days + 1
     price *= (1 + price_change)
-    print(f"${price:,.2f}")
+    print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
+
+out_file.close()
