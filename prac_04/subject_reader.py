fc8c20b6e26ed4c0d4302c431c75412361f02ee4
harrisonroufos <harrison.roufos@my.jcu.edu.au>
2023-06-22 13:04:07 +1000

    Add display subject details function and fix get data function for subject reader program

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -9,21 +9,27 @@ FILENAME = "subject_data.txt"
 def main():
     data = get_data()
     print(data)
+    display_subject_details(data)
+
+
+def display_subject_details(subject_data: list):
+    """Display subject details from list"""
+    for subject, teacher, number_of_students in subject_data:
+        print(f"{subject} is taught by {teacher:12} and has {number_of_students:4} students")
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    subject_data = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        line = line.strip()
+        parts = line.split(",")
+        parts[2] = int(parts[2])
+        subject_data.append(parts)
     input_file.close()
+    return subject_data
 
 
 main()
+
