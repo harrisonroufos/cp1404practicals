fdbc14bcd761ee5e550fdec7d330cc0440fcaae4
harrisonroufos <harrison.roufos@my.jcu.edu.au>
2023-06-22 15:28:42 +1000

    Fixed formatting and print in ascending picks order for quick picks program

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -2,6 +2,7 @@
 CP1404 Practical
 """
 import random
+from operator import itemgetter
 
 MAX_NUMBER = 45
 MIN_NUMBER = 1
@@ -9,8 +10,16 @@ NUMBERS_PER_LINE = 6
 
 
 def main():
-    numbers = generate_quick_pick_numbers(MIN_NUMBER, MAX_NUMBER, NUMBERS_PER_LINE)
-    print(numbers)
+    """Prints quick picks numbers in ascending order"""
+    picks = []
+    number_of_picks = int(input("How many quick picks? "))
+    for pick in range(number_of_picks):
+        picks.append(generate_quick_pick_numbers(MIN_NUMBER, MAX_NUMBER, NUMBERS_PER_LINE))
+    picks.sort(key=itemgetter(-1))
+    for pick in picks:
+        for part in pick:
+            print(f"{part:2}", end=" ")
+        print("")
 
 
 def generate_quick_pick_numbers(min_number, max_number, number_of_numbers):
@@ -21,7 +30,7 @@ def generate_quick_pick_numbers(min_number, max_number, number_of_numbers):
         while random_number in numbers:
             random_number = random.randint(min_number, max_number)
         numbers.append(random_number)
-        numbers.sort()
+    numbers.sort()
     return numbers
 
 
