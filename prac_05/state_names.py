6a37ae1cac72e04e215b6b48bb6a4eeebc5cfc67
harrisonroufos <harrison.roufos@my.jcu.edu.au>
2023-06-29 12:51:57 +1000

    Add for loop through code to names in state names program

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -9,6 +9,9 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
+for code, name in CODE_TO_NAME.items():
+    print(f"{code:3} is {name}")
+
 state_code = input("Enter short state: ").upper()
 while state_code != "":
     if state_code in CODE_TO_NAME:
