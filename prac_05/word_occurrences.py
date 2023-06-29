814083efeff9b54f41d5c5de58674f156077381d
harrisonroufos <harrison.roufos@my.jcu.edu.au>
2023-06-29 14:16:24 +1000

    Add formatting to word occurrences program

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -2,7 +2,7 @@
 CP1404 Practical
 Word Occurrences
 Estimate: 20 minutes
-Actual:
+Actual:   28 minutes
 """
 word_to_count = {}
 text = input("Text: ")
@@ -13,5 +13,12 @@ for word in words:
     else:
         word_to_count[word] += 1
 
+max_length = 0
+for word in words:
+    if len(word) > max_length:
+        max_length = len(word)
+
+print(max_length)
+
 for word, count in word_to_count.items():
-    print(f"{word} : {count}")
+    print(f"{word:{max_length}} : {count}")
