d2c27790ef77dbe4f49b94a0d936812263ce54d8
harrisonroufos <harrison.roufos@my.jcu.edu.au>
2023-07-12 21:20:27 +1000

    Add __str__ method to car.py and add Car object called limo to used.cars.py

--- prac_06/car.py
+++ prac_06/car.py
@@ -4,13 +4,17 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, fuel=0, name=""):
         """Initialise a Car instance.
-
         fuel: float, one unit of fuel drives one kilometre
         """
         self.fuel = fuel
         self._odometer = 0
+        self.name = name
+
+    def __str__(self):
+        """Return string representation of name, fuel and odometer of a vehicle"""
+        return f"{self.name}, fuel={self.fuel}, odometer={self._odometer}"
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
@@ -18,7 +22,6 @@ class Car:
 
     def drive(self, distance):
         """Drive the car a given distance.
-
         Drive given distance if car has enough fuel
         or drive until fuel runs out return the distance actually driven.
         """
