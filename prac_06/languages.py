a4733c1d5bbf39b19770f111c9a09bbd7607d6ae
harrisonroufos <harrison.roufos@my.jcu.edu.au>
2023-07-12 22:11:41 +1000

    Add data to languages.py

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -4,3 +4,12 @@ languages
 Estimate: 25 minutes
 Actual:
 """
+from prac_06.programming_language import ProgrammingLanguage
+
+# test = ProgrammingLanguage("Test", "Dynamic", True, 1991)
+# print(test.is_dynamic())
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
