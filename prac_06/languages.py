d9a92cb2d4863e61ec33533dfd186bcfdfcd96c9
harrisonroufos <harrison.roufos@my.jcu.edu.au>
2023-07-12 22:19:07 +1000

    Add data to programming_languages_data list in languages.py

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -13,3 +13,10 @@ python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
 ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
 visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 print(python)
+
+
+programming_languages_data = [ProgrammingLanguage("Python", "Dynamic", True, 1991),
+                              ProgrammingLanguage("Ruby", "Dynamic", True, 1995),
+                              ProgrammingLanguage("Visual Basic", "Static", False, 1991)]
+
+
