9e435ce8defd1460f1cf723baf4bdf1be5d0f452
harrisonroufos <harrison.roufos@my.jcu.edu.au>
2023-07-13 18:12:34 +1000

    Update docstrings in Guitar class in guitar.py

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -10,15 +10,24 @@ class Guitar:
     """Represent a Guitar object."""
 
     def __init__(self, name="", year=0, cost=0):
+        """
+        Initialise a Guitar instance
+        name: name of guitar
+        year: year guitar was made
+        cost: amount of money the guitar cost
+        """
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
+        """Return string representation of name, (year made) and cost of a guitar"""
         return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
 
     def get_age(self):
+        """Returns age of guitar from 2023"""
         return 2023 - self.year
 
     def is_vintage(self):
+        """Returns if guitar is vintage if its over 50 years old"""
         return self.get_age() > 50
