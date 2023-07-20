d133b7531baf73cde3792ac9ad07a068f3752308
harrisonroufos <harrison.roufos@my.jcu.edu.au>
2023-07-20 18:33:48 +1000

    Add menu while loop with load_file function and save_to_file function in project management program

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -7,24 +7,49 @@ Actual:
 
 from project import Project
 
+MENU = "(L)oad projects\n(S)ave projects\n(D)isplay projects\n(F)ilter projects by da\n(A)dd new project\n(U)pdate project\n(Q)uit"
 IN_FILE = "project.txt"
 OUT_FILE = "test.txt"
 
-projects = []
-in_file = open(IN_FILE, "r")
-header = in_file.readline()
 
-for line in in_file:
-    parts = line.strip().split("\t")
-    projects.append(Project(parts[0], parts[1], int(parts[2]), float(parts[3]), int(parts[4])))
+def main():
+    print(MENU)
+    choice = input(">>> ").lower()
+    while choice != "q":
+        if choice == "l":
+            header, projects = load_file()  # header is for when saving to file for first line
+        elif choice == "s":
+            save_to_file(header, projects)
+        else:
+            print("Invalid")
+        print(MENU)
+        choice = input(">>> ").lower()
+    save_to_file(header, projects)
 
-for project in projects:
-    print(project)
 
-in_file.close()
+def load_file():
+    projects = []
+    file_name = input("File name: ")
+    in_file = open(file_name, "r")
+    header = in_file.readline()
+    for line in in_file:
+        parts = line.strip().split("\t")
+        projects.append(Project(parts[0], parts[1], int(parts[2]), float(parts[3]), int(parts[4])))
+    in_file.close()
+    print(f"{file_name} loaded.")
+    return header, projects
 
-out_file = open(OUT_FILE, "w")
-print(header.strip("\n"), file=out_file)
-for project in projects:
-    print(f"{project.name}\t{project.start_date}\t{project.priority}\t{project.cost_estimate:.1f}\t{project.completion_percentage}", file=out_file)
-out_file.close()
+
+def save_to_file(header, projects):
+    file_name = input("File name to save to: ")
+    out_file = open(file_name, "w")
+    print(header.strip("\n"), file=out_file)
+    for project in projects:
+        print(
+            f"{project.name}\t{project.start_date}\t{project.priority}\t{project.cost_estimate:.1f}\t{project.completion_percentage}",
+            file=out_file)
+    out_file.close()
+    print(f"Saved to {file_name}.")
+
+
+main()
