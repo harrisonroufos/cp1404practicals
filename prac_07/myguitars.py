6a5c8b621222a3931b0514e4ba4357de02aeb23f
harrisonroufos <harrison.roufos@my.jcu.edu.au>
2023-07-20 15:40:37 +1000

    Add guitars csv file and add my guitars program

---
+++ prac_07/myguitars.py
@@ -0,0 +1,18 @@
+"""
+CP1404 Practical
+my guitars
+"""
+
+from prac_06.guitar import Guitar
+import csv
+
+FILE_NAME = "guitars.csv"
+
+guitars = []
+in_file = open(FILE_NAME, "r")
+reader = csv.reader(in_file)
+for line in reader:
+    guitars.append(Guitar([line[0]], int(line[1]), float(line[2])))
+
+for guitar in guitars:
+    print(guitar)
