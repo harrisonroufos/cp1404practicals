71b442cc5ed6ac4bb405e9ba980084b36c05ed49
harrisonroufos <harrison.roufos@my.jcu.edu.au>
2023-07-27 20:12:53 +1000

    Update docstrings for box layout demo program

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -8,16 +8,19 @@ from kivy.lang import Builder
 
 class BoxLayoutDemo(App):
     def build(self):
+        """Build the Kivy GUI"""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
+        """Changes output_label text to greet input_name text"""
         print("greet")
         print("test")
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def handle_clear(self):
+        """Clears output_label and input_name texts"""
         self.root.ids.output_label.text = ""
         self.root.ids.input_name.text = ""
 
