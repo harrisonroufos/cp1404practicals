d4b784f2aaec60b43951421f1d0dacdb23397991
harrisonroufos <harrison.roufos@my.jcu.edu.au>
2023-07-27 21:55:09 +1000

    Updated convert miles km kv to convert on text and convert miles km program docstrings

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -9,10 +9,12 @@ MILE_TO_KILOMETRE_CONVERSION = 1.60934
 
 
 class ConvertMilesToKilometres(App):
+    """ ConvertMilesToKilometres is a Kivy App for converting miles to kilometres """
     conversion_result = StringProperty()
     user_input_number = StringProperty()
 
     def build(self):
+        """Build the Kivy GUI"""
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         self.conversion_result = ""
@@ -20,6 +22,7 @@ class ConvertMilesToKilometres(App):
         return self.root
 
     def handle_conversion(self, miles):
+        """Converts miles to kilometres"""
         try:
             conversion = float(miles) * MILE_TO_KILOMETRE_CONVERSION
             self.conversion_result = str(conversion)
@@ -27,6 +30,7 @@ class ConvertMilesToKilometres(App):
             self.conversion_result = "0.0"
 
     def handle_increment(self, miles, increment):
+        """Changes miles input by -1 or 1 increments"""
         try:
             miles_incremented = float(miles) + increment
             self.user_input_number = str(miles_incremented)
