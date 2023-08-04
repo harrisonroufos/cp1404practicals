092075b367210151ee3fa47d00dce8c81057e68a
harrisonroufos <harrison.roufos@my.jcu.edu.au>
2023-08-04 18:38:03 +1000

    Update docstrings in SilverServiceTaxi class and UnreliableCar class

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -7,17 +7,20 @@ from taxi import Taxi
 
 
 class SilverServiceTaxi(Taxi):
+    """Specialised version of a Taxi that includes fanciness multiplier and flagfall."""
     flagfall = 4.50
 
     def __init__(self, name, fuel, fanciness: float):
+        """Initialise a SilverServiceTaxi instance, based on parent class Taxi."""
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km = self.price_per_km * fanciness
 
     def __str__(self):
+        """Return a string like a Taxi but with flagfall."""
         return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
 
     def get_fare(self):
-        """Return the price for the taxi trip."""
+        """Return the price for the taxi trip plus flagfall."""
         return super().get_fare() + self.flagfall
 
--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -6,13 +6,17 @@ UnreliableCar class
 from car import Car
 import random
 
+
 class UnreliableCar(Car):
+    """Specialised version of a Car that includes reliability chance."""
 
     def __init__(self, name, fuel, reliability: float):
+        """Initialise a UnreliableCar instance, based on parent class Car."""
         super().__init__(name, fuel)
         self.reliability = reliability
 
     def drive(self, distance):
+        """Return the distance driven for the car if the reliability is more than a random number between 0-100."""
         if self.reliability > random.randint(0, 100):
             distance_driven = super().drive(distance)
             return distance_driven
